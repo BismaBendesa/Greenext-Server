62c11e242c955610f0899795130a7d317cfa0797
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-12-15 10:14:29 +0800

    Create get admins api

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,29 @@ const getUsers = async (req, res) => {
   try {
     const query = `SELECT * FROM users`;
     const [result] = await db.query(query);
-    res.status(200).json({ success: true, data: result });
+    res
+      .status(200)
+      .json({ success: true, data: result, message: "Get Users Successfull" });
   } catch (error) {
     console.error("Error get user data.", error.message);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
+const getAdmins = async (req, res) => {
+  try {
+    const query = "SELECT * FROM users WHERE role = 'admin'";
+    const [result] = await db.query(query);
+    res
+      .status(200)
+      .json({ success: true, message: "Get admin successful", data: result });
+  } catch (error) {
+    console.error("Error Get Admin", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   getUsers,
+  getAdmins,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUsers } = require("../controllers/userController");
+const { getUsers, getAdmins } = require("../controllers/userController");
 
 router.get("/", getUsers); //http://localhost:5000/api/v1/user
+router.get("/admin", getAdmins); //http://localhost:5000/api/v1/user/admin
 
 module.exports = router;
